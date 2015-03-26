cd7582b47cd898f78f7b2eee2d4d47ffc219e007
1Rhino <buikhanh.bk@gmail.com>
2015-03-26 16:15:48 +0900

    finish exercise

--- rhino/lib/rhino.rb
+++ rhino/lib/rhino.rb
@@ -1,8 +1,10 @@
+require "rhino/array"
 require "rhino/version"
 
 module Rhino
   class Application
     def call(env)
+      `echo debug > debug.txt`
       [200, {'Content-Type' => 'text/html'}, ['Hello world from Rhino Framework!']]
     end
   end
